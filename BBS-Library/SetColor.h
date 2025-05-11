29e51d8bc97ae3388210e9d25e016d6890437d44
Never <never2137922856@gmail.com>
2025-05-12 01:02:00 +0800

    Create SetColor.h

---
+++ BBS-Library/SetColor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <windows.h>
+
+namespace SetColor {
+    // set the color of the word
+    void setColor(WORD color);
+
+    // Common color definitions
+    enum Color {
+        BLACK = 0,
+        BLUE = 1,
+        GREEN = 2,
+        CYAN = 3,
+        RED = 4,
+        PURPLE = 5,
+        YELLOW = 6,
+        WHITE = 7,
+        GRAY = 8,
+        LIGHT_BLUE = 9,
+        LIGHT_GREEN = 10,
+        LIGHT_CYAN = 11,
+        LIGHT_RED = 12,
+        LIGHT_PURPLE = 13,
+        LIGHT_YELLOW = 14,
+        BRIGHT_WHITE = 15
+    };
+}
