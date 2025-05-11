d2c1015f664588418aa2aa86320ef6aaf38ddce1
Cascade Kobayashi <38118922+Isoheptane@users.noreply.github.com>
2025-05-11 12:23:37 +0800

    Create BBS Client / Server Project

--- BBS-Client/BBS-Client.cpp
+++ BBS-Client/BBS-Client.cpp
@@ -1,26 +1,34 @@
 ﻿// BBS-Client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
+#include <cstdio>
+#include <cstdint>
 #include <iostream>
+#include <string>
 
 #include "Crypto.h"
 #include "SocketW.h"
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+using WinSock::SocketW;
+
 int main()
 {
-    std::cout << "Hello World!\n";
-    Crypto::AES::example("test\n");
-    SocketW s = SocketW();
-    s.example_func();
+    SocketW client = SocketW();
+    client.init();
+    client.connect("127.0.0.1", 11451);
+
+    while (true) {
+        printf("Your Message: ");
+        string data;
+        cin >> data;
+        uint16_t len = data.length();
+        client.send((char*)(&len), 2);
+        client.send(data.c_str(), len);
+    }
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ BBS-Server/BBS-Server.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+#include "Crypto.h"
+#include "SocketW.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+using WinSock::SocketW;
+
+int main()
+{
+	SocketW listener = SocketW();
+	listener.init();
+	listener.bind("0.0.0.0", 11451);
+	listener.listen(16);
+
+	cout << "Listening.." << endl;
+
+	SocketW client;
+	if (listener.accept(&client) != 0) {
+		cout << "Failed to accept client" << endl;
+		return -1;
+	}
+	else {
+		cout << "Client accepted" << endl;
+	}
+
+	while (true) {
+		uint16_t length = 0;
+		char buffer[4096];
+		if (client.recvAll((char*)(&length), 2) == -1) {
+			cout << "Failed to receive message head" << endl;
+			return -1;
+		}
+		if (client.recvAll(buffer, length) == -1) {
+			cout << "Failed to receive message body" << endl;
+			return -1;
+		}
+		buffer[length] = '\0';
+		cout << "Length " << length << " received,\nMessage: " << buffer << endl;
+	}
+}
