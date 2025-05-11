233dd50008a069bf75548bf5c07f276698a5e8e8
Cascade Kobayashi <38118922+Isoheptane@users.noreply.github.com>
2025-05-11 11:08:32 +0800

    Wrap SocketW in a namespace

--- BBS-Library/SocketW.cpp
+++ BBS-Library/SocketW.cpp
@@ -1,134 +1,154 @@
 #include "pch.h"
+
+/*
+    I know this is not good, but anyway, this 
+    can still be used since we wrapped it in 
+    our SocketW class.
+*/
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "SocketW.h"
 
 #include <cstdio>
 #include <string.h>
 
-WSAData g_wsaData;
-bool g_wsaInitialized = false;
+namespace WinSock {
+    WSAData g_wsaData;
+    bool g_wsaInitialized = false;
 
 
-bool InitializeWSA() {
-    if (!g_wsaInitialized) {
-        if (WSAStartup(MAKEWORD(2, 2), &g_wsaData) != 0) {
-            fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
-            return false;
+    bool InitializeWSA() {
+        if (!g_wsaInitialized) {
+            if (WSAStartup(MAKEWORD(2, 2), &g_wsaData) != 0) {
+                fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
+                return false;
+            }
+            g_wsaInitialized = true;
         }
-        g_wsaInitialized = true;
-    }
-    return true;
-}
-
-
-void CleanupWSA() {
-    if (g_wsaInitialized) {
-        WSACleanup();
-        g_wsaInitialized = false;
-    }
-}
-
-SocketW::SocketW() : sock(INVALID_SOCKET) {
-    
-}
-
-SocketW::~SocketW() {
-    // only clean up the socket if it was created
-    if (sock != INVALID_SOCKET) {
-        closesocket(sock);
-        sock = INVALID_SOCKET;
-    }
-    
-}
-
-int SocketW::createsck() {
-    // make sure WSA is initialized before creating a socket
-    if (!g_wsaInitialized && !InitializeWSA()) {
-        return -1;
-    }
-    
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET) {
-        fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
-        return -1;
-    }
-    return 0;
-}
-
-int SocketW::bindsck(const char *ip, int port) {
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    
-    if (bind(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        fprintf(stderr, "bind() failed with error %d\n", WSAGetLastError());
-        return -1;
-    }
-    return 0;
-}
-
-int SocketW::listensck(int backlog) {
-    if (listen(sock, backlog) == SOCKET_ERROR) {
-        fprintf(stderr, "listen() failed with error %d\n", WSAGetLastError());
-        return -1;
-    }
-    return 0;
-}
-
-int SocketW::acceptsck(SOCKET *clientSocket) {
-    struct sockaddr_in clientAddr;
-    int addrLen = sizeof(clientAddr);
-    SOCKET clientSock = accept(sock, (struct sockaddr*)&clientAddr, &addrLen);
-    if (*clientSocket == INVALID_SOCKET) {
-        fprintf(stderr, "accept() failed with error %d\n", WSAGetLastError());
-        return -1;
-    }
-    return 0; 
-}
-
-int SocketW::connectToServer(const char* serverName, unsigned short port) {
-    struct hostent* hp;
-    if (isalpha(serverName[0])) {
-        hp = gethostbyname(serverName);
-    } else {
-        unsigned int addr = inet_addr(serverName);
-        hp = gethostbyaddr((char*)&addr, 4, AF_INET);
+        return true;
     }
 
-    if (hp == NULL) {
-        fprintf(stderr, "Cannot resolve address: %d\n", WSAGetLastError());
-        return -1;
+    void CleanupWSA() {
+        if (g_wsaInitialized) {
+            WSACleanup();
+            g_wsaInitialized = false;
+        }
     }
 
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    memcpy(&(serverAddr.sin_addr), hp->h_addr, hp->h_length);
-    serverAddr.sin_family = hp->h_addrtype;
-    serverAddr.sin_port = htons(port);
-
-    if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        fprintf(stderr, "connect() failed with error %d\n", WSAGetLastError());
-        return -1;
+    SocketW::SocketW() : sock(INVALID_SOCKET) {
+        memset(&sockAddr, 0, sizeof(sockAddr));
+        memset(&peerAddr, 0, sizeof(peerAddr));
     }
-    return 0;
-}
 
-int SocketW::sendsck(const char* buffer, int length) {
-    int bytesSent = send(sock, buffer, length, 0);
-    if (bytesSent == SOCKET_ERROR) {
-        fprintf(stderr, "send() failed with error %d\n", WSAGetLastError());
-        return -1;
+    SocketW::SocketW(SOCKET socket) : sock(socket) {
+        memset(&sockAddr, 0, sizeof(sockAddr));
+        memset(&peerAddr, 0, sizeof(peerAddr));
     }
-    return bytesSent;
-}
 
-int SocketW::recvsck(char* buffer, int length) {
-    int bytesRecv = recv(sock, buffer, length, 0);
-    if (bytesRecv == SOCKET_ERROR) {
-        fprintf(stderr, "recv() failed with error %d\n", WSAGetLastError());
-        return -1;      
-    } else if (bytesRecv > 0) {
-        buffer[bytesRecv] = '\0'; // Null-terminate the received string
-    } else if (bytesRecv == 0) {
-        printf("Connection closed\n");
+    SocketW::~SocketW() {
+        // only clean up the socket if it was created
+        if (sock != INVALID_SOCKET) {
+            closesocket(sock);
+            sock = INVALID_SOCKET;
+        }
     }
-    return bytesRecv;
+
+    int SocketW::init() {
+        // make sure WSA is initialized before creating a socket
+        if (!g_wsaInitialized && !InitializeWSA()) {
+            return -1;
+        }
+
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == INVALID_SOCKET) {
+            fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
+            return -1;
+        }
+        return 0;
+    }
+
+    int SocketW::bind(const char* ip, int port) {
+        sockAddr.sin_family = AF_INET;
+        sockAddr.sin_port = htons(port);
+        sockAddr.sin_addr.s_addr = INADDR_ANY;
+        
+        if (::bind(this->sock, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) == SOCKET_ERROR) {
+            fprintf(stderr, "bind() failed with error %d\n", WSAGetLastError());
+            return -1;
+        }
+        return 0;
+    }
+
+    int SocketW::listen(int backlog) {
+        if (::listen(sock, backlog) == SOCKET_ERROR) {
+            fprintf(stderr, "listen() failed with error %d\n", WSAGetLastError());
+            return -1;
+        }
+        return 0;
+    }
+
+    int SocketW::accept(SocketW* sockw) {
+        struct sockaddr_in clientAddr;
+        int addrLen = sizeof(clientAddr);
+
+        SOCKET newSock = ::accept(sock, (struct sockaddr*)&clientAddr, &addrLen);
+        if (newSock == INVALID_SOCKET) {
+            fprintf(stderr, "accept() failed with error %d\n", WSAGetLastError());
+            return -1;
+        }
+        *sockw = SocketW(newSock);
+        sockw->peerAddr = clientAddr;
+        return 0;
+    }
+
+    int SocketW::connect(const char* serverName, unsigned short port) {
+        struct hostent* hp;
+        if (isalpha(serverName[0])) {
+            hp = gethostbyname(serverName);
+        }
+        else {
+            unsigned int addr = inet_addr(serverName);
+            hp = gethostbyaddr((char*)&addr, 4, AF_INET);
+        }
+
+        if (hp == NULL) {
+            fprintf(stderr, "Cannot resolve address: %d\n", WSAGetLastError());
+            return -1;
+        }
+
+        memset(&peerAddr, 0, sizeof(peerAddr));
+        memcpy(&(peerAddr.sin_addr), hp->h_addr, hp->h_length);
+        peerAddr.sin_family = hp->h_addrtype;
+        peerAddr.sin_port = htons(port);
+
+        if (::connect(sock, (struct sockaddr*)&peerAddr, sizeof(peerAddr)) == SOCKET_ERROR) {
+            fprintf(stderr, "connect() failed with error %d\n", WSAGetLastError());
+            return -1;
+        }
+        return 0;
+    }
+
+    int SocketW::send(const char* buffer, int length) {
+        int bytesSent = ::send(sock, buffer, length, 0);
+        if (bytesSent == SOCKET_ERROR) {
+            fprintf(stderr, "send() failed with error %d\n", WSAGetLastError());
+            return -1;
+        }
+        return bytesSent;
+    }
+
+    int SocketW::recv(char* buffer, int length) {
+        int bytesRecv = ::recv(sock, buffer, length, 0);
+        if (bytesRecv == SOCKET_ERROR) {
+            fprintf(stderr, "recv() failed with error %d\n", WSAGetLastError());
+            return -1;
+        }
+        else if (bytesRecv > 0) {
+            buffer[bytesRecv] = '\0'; // Null-terminate the received string
+        }
+        else if (bytesRecv == 0) {
+            printf("Connection closed\n");
+        }
+        return bytesRecv;
+    }
+
 }
--- BBS-Library/SocketW.h
+++ BBS-Library/SocketW.h
@@ -1,36 +1,36 @@
 #pragma once
 
-#ifndef SOCKETW_H
-#define SOCKETW_H
-
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 #include <ws2tcpip.h>
 
-// global WSAData
-extern WSAData g_wsaData;
-extern bool g_wsaInitialized;
+namespace WinSock {
+    // global WSAData
+    extern WSAData g_wsaData;
+    extern bool g_wsaInitialized;
 
-// WSA initialization and cleanup functions
-bool InitializeWSA();
-void CleanupWSA();
+    // WSA initialization and cleanup functions
+    bool InitializeWSA();
+    void CleanupWSA();
 
-class SocketW
-{
-private:
-    SOCKET sock;
-    struct sockaddr_in serverAddr;
+    class SocketW
+    {
+    private:
+        SOCKET sock;
+        sockaddr_in sockAddr;
+        sockaddr_in peerAddr;
 
-public:
-    SocketW();
-    ~SocketW();
-    
-    int createsck();
-    int bindsck(const char* ip, int port);
-    int listensck(int backlog);
-    int acceptsck(SOCKET* clientSocket);
-    int connectToServer(const char* serverName, unsigned short port);
-    int sendsck(const char* buffer, int length);
-    int recvsck(char* buffer, int length);
-};
-#endif
+    public:
+        SocketW();
+        SocketW(SOCKET socket);
+        ~SocketW();
+
+        int init();
+        int bind(const char* ip, int port);
+        int listen(int backlog);
+        int accept(SocketW* sockw);
+        int connect(const char* serverName, unsigned short port);
+        int send(const char* buffer, int length);
+        int recv(char* buffer, int length);
+    };
+}
